78acc5161bbd87ec3103559ebe702df2e0618e3e
Alexis Narvaez Ruiz <zaizealexis@gmail.com>
2021-11-04 09:10:31 -0600

    sdcsadcasdc

--- database/migrations/2021_06_22_000000_create_citas_table.php
+++ database/migrations/2021_06_22_000000_create_citas_table.php
@@ -23,7 +23,7 @@ class CreateCitasTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('username')->unique();
-            $table->longText('photo');
+            $table->longText('photo')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
